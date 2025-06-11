04c14db9e5863d6acd2a131d1d93e9cbbcaa24e5
zeryzez <lolmitoman@gmail.com>
2025-06-11 10:45:49 +0200

    feat eventcard

--- src/views/eventDetails.ts
+++ src/views/eventDetails.ts
@@ -3,7 +3,4 @@ export const renderEventDetail = (app: HTMLElement, paramId: string | undefined)
     const pageContent = homePageTemplate({ eventsContent: eventsHtml });
 
     app.innerHTML = layoutTemplate({ content: pageContent });
-
-
-
 }
--- src/views/home.ts
+++ src/views/home.ts
@@ -3,13 +3,18 @@ import { loadTemplate, initializePartials } from '@/utils/templateUtils';
 
 export async function renderHome(app: HTMLElement) {
 
+    const events = await fetchEvents();
+
     // Initialiser les partiels
     await initializePartials();
     const layoutTemplate = await loadTemplate('/src/templates/layout.hbs');
-
+    const eventCardTemplate = await loadTemplate('/src/components/eventCard.hbs');
     const homePageTemplate = await loadTemplate('/src/templates/pages/home.hbs');
 
-    const pageContent = homePageTemplate({ eventsContent: '' });
+    // Récupérer les événements
+    const eventsHTML = events.map(event => eventCardTemplate(event)).join('');
+
+    const pageContent = homePageTemplate({ eventsContent: eventsHTML });
 
     app.innerHTML = layoutTemplate({ content: pageContent });
 
