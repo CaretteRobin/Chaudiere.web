eb4645808a5bb5fddcff093450ec982ef7199d4a
zeryzez <lolmitoman@gmail.com>
2025-06-12 11:21:05 +0200

    feat:filtered event on categ

--- src/views/events.ts
+++ src/views/events.ts
@@ -1,4 +1,5 @@
 import { fetchEventsCurrent } from '@/api/events';
+import { fetchCategories } from '@/api/categories';
 import { loadTemplate, initializePartials } from '@/utils/templateUtils';
 
 export async function renderEvent(app: HTMLElement) {
@@ -14,11 +15,34 @@ export async function renderEvent(app: HTMLElement) {
         app.innerHTML = layoutTemplate({ content: '<p>Impossible de charger les événements.</p>' });
         return;
     }
+    let categories = [];
+    try {
+        categories = await fetchCategories();
+    } catch (error) {
+        console.error('Erreur lors du chargement des catégories:', error);
+        app.innerHTML = layoutTemplate({ content: '<p>Impossible de charger les catégories.</p>' });
+        return;
+    }
+
+    const params = new URLSearchParams(window.location.search);
+    const selectedCategory = params.get('category') ?? '';
+
+    const filteredEvents = selectedCategory
+        ? events.filter(e => e.category === selectedCategory)
+        : events;
 
     const eventCardTemplate = await loadTemplate('/src/components/eventCard.hbs');
     const eventsPageTemplate = await loadTemplate('/src/templates/pages/events.hbs');
+    const categoriesFilterTemplate = await loadTemplate('/src/components/categoriesFilter.hbs');
+    const categoriesFilterHtml = categoriesFilterTemplate({
+        categories: categories.map(category => ({
+            ...category,
+            selected: category.name === selectedCategory
+        })),
+        selectedCategory
+    });
 
-    const eventsHtml = events.map(event => {
+    const eventsHtml = filteredEvents.map(event => {
         if (event.url) {
             // couper les 4 premiers caractères de l'URL qui correspondent à "/api"
             event.url = event.url.substring(4);
@@ -28,10 +52,7 @@ export async function renderEvent(app: HTMLElement) {
 
     // const eventsHtml = events.map(event => eventCardTemplate(event)).join('');
 
-    console.log(eventsHtml);
-
-    const eventsPage = eventsPageTemplate({ eventsContent: eventsHtml });
-    console.log(eventsPage);
+    const eventsPage = eventsPageTemplate({categFilter: categoriesFilterHtml, eventsContent: eventsHtml });
 
     app.innerHTML = layoutTemplate({ content: eventsPage });
 }
